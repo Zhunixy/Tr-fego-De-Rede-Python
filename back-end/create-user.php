28380a484bc880485df7c3e01f3f3af946c247a5
wlademirAntonio <tonymartinsjunior@gmail.com>
2025-03-21 21:40:57 -0300

    bd e back-end

---
+++ back-end/create-user.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once 'db.php';
+
+$name = 'Wlademir';
+$email = 'tonymartinsjunior@gmail.com';
+$password = md5('123465');
+
+if (empty($name) || empty($email) || empty($password)) {
+    $data = array(
+        'type' => 'error',
+        'message' => 'Campo(s) obrigatório(s) não preenchido(s).'
+    );
+} else {
+
+    try {
+        $sql = "insert into user values (null, ?, ?, ?)";
+        $sql = DB::prepare($sql);
+        $sql->execute([$name, $email, $password]);
+
+        $data = array(
+            'type' => 'success',
+            'message' => 'Cadastro realizado com sucesso!'
+        );
+    } catch (PDOException $e) {
+        $data = array(
+            'type' => 'error',
+            'message' => 'Falha ao realizar o cadastro.'
+        );
+    }
+}
+
+echo json_encode($data)
+
+?>
---
+++ proj_dponet.sql
@@ -0,0 +1,25 @@
+-- Criar o banco de dados "proj_dponet"
+create database if not exists `proj_dponet`;
+
+-- Usar o banco de dados "proj_dponet"
+use `proj_dponet`;
+
+-- Criar a tabela "user" dentro do banco de dados "proj_dponet"
+create table if not exists `proj_dponet`.`user` (
+`id` int auto_increment, -- Atributo "id", tipo inteiro, preenchimento automático
+`name` varchar(45) not null, -- Atributo "name", tipo texto, 45 caracteres, não pode ser vazio
+`email` varchar(45) not null unique, -- Atributo "email", tipo texto, 45 caracteres, não pode ser vazio
+`password` varchar(100) not null, -- Atributo "password", tipo texto, 100 caracteres, não pode ser vazio
+primary key (`id`) -- Declaração da chave primária
+);
+
+-- Criar a tabela "history" dentro do banco de dados "proj_dponet"
+create table if not exists `proj_dponet`.`history` (
+`id` int auto_increment, -- Atributo "id", tipo inteiro, preenchimento automático
+`date`date not null, -- Atributo "date", tipo data, não pode ser vazio
+`description` varchar(45) not null, -- Atributo "description", tipo texto, 45 caracteres, não pode ser vazio
+`document` varchar(45) not null unique, -- Atributo "document", tipo texto, 45 caracteres, não pode ser vazio
+`user_id` int not null, -- Atributo "user_id", tipo inteiro, não pode ser vazio
+primary key (`id`), -- Declaração da chave primária
+foreign key (`user_id`) references `user`(`id`) -- Declaração de chave estrangeira
+);
