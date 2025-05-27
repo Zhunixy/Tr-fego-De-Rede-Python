91e2ac9812e3cb4ad997d8a05c0f28bfe2f59a39
wlademirAntonio <tonymartinsjunior@gmail.com>
2025-05-27 13:36:33 -0300

    atualizacao db

--- proj_dponet.sql
+++ proj_dponet.sql
@@ -8,7 +8,9 @@ use `proj_dponet`;
 create table if not exists `proj_dponet`.`user` (
 `id` int auto_increment, -- Atributo "id", tipo inteiro, preenchimento automático
 `name` varchar(45) not null, -- Atributo "name", tipo texto, 45 caracteres, não pode ser vazio
-`email` varchar(45) not null unique, -- Atributo "email", tipo texto, 45 caracteres, não pode ser vazio
+`cpf_cnpj` varchar(45) not null unique, -- Atributo "ident", tipo texto, 45 caracteres, não pode ser vazio, unico
+`telefone` varchar(45) not null unique, -- Atributo "telefone", tipo texto, 45 caracteres, não pode ser vazio, unico
+`email` varchar(45) not null unique, -- Atributo "email", tipo texto, 45 caracteres, não pode ser vazio, unico
 `password` varchar(100) not null, -- Atributo "password", tipo texto, 100 caracteres, não pode ser vazio
 primary key (`id`) -- Declaração da chave primária
 );
@@ -18,10 +20,10 @@ create table if not exists `proj_dponet`.`history` (
 `id` int auto_increment, -- Atributo "id", tipo inteiro, preenchimento automático
 `date`date not null, -- Atributo "date", tipo data, não pode ser vazio
 `description` varchar(45) not null, -- Atributo "description", tipo texto, 45 caracteres, não pode ser vazio
-`document` varchar(45) not null unique, -- Atributo "document", tipo texto, 45 caracteres, não pode ser vazio
+`document` varchar(45) not null unique, -- Atributo "document", tipo texto, 45 caracteres, não pode ser vazio, unico
 `user_id` int not null, -- Atributo "user_id", tipo inteiro, não pode ser vazio
 primary key (`id`), -- Declaração da chave primária
 foreign key (`user_id`) references `user`(`id`) -- Declaração de chave estrangeira
 );
 
-INSERT INTO `USER` VALUEs (NULL, 'Teste', 'teste@teste', 'teste');
+INSERT INTO `USER` VALUEs (NULL, 'Teste', 99999999999, 99999999999, 'teste@teste', 'teste');
